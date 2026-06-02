4fc9ae4851df7eae0c983d922d7c4521e5ebd2b4
over <ov3rline@proton.me>
2026-06-02 18:00:52 -0300

    Criar formulários de consulta concretos

---
+++ Forms/Consultas/ConsultaCidades.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test.Forms.Consultas
+{
+    public partial class ConsultaCidades : Test.Consulta
+    {
+        public ConsultaCidades()
+        {
+            InitializeComponent();
+        }
+    }
+}
