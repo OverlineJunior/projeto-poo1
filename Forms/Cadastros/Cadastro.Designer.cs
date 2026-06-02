7a4433a4278b022af2914ef0c19bd23f9340d777
over <ov3rline@proton.me>
2026-06-02 20:28:48 -0300

    Adicionar labels de data de cadastro, última alteração e usuário no formulário base de cadastro

--- Forms/Cadastros/Cadastro.Designer.cs
+++ Forms/Cadastros/Cadastro.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.btnSalvar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDataCadastro = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDataAlteracao = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSalvar
@@ -41,16 +47,85 @@
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label2.Location = new System.Drawing.Point(12, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Data de cadastro:";
+            // 
+            // txtDataCadastro
+            // 
+            this.txtDataCadastro.AutoSize = true;
+            this.txtDataCadastro.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtDataCadastro.Location = new System.Drawing.Point(110, 425);
+            this.txtDataCadastro.Name = "txtDataCadastro";
+            this.txtDataCadastro.Size = new System.Drawing.Size(0, 13);
+            this.txtDataCadastro.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label4.Location = new System.Drawing.Point(208, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Última alteração:";
+            // 
+            // txtDataAlteracao
+            // 
+            this.txtDataAlteracao.AutoSize = true;
+            this.txtDataAlteracao.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtDataAlteracao.Location = new System.Drawing.Point(300, 425);
+            this.txtDataAlteracao.Name = "txtDataAlteracao";
+            this.txtDataAlteracao.Size = new System.Drawing.Size(0, 13);
+            this.txtDataAlteracao.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label6.Location = new System.Drawing.Point(404, 425);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Usuário:";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.AutoSize = true;
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.txtUsuario.Location = new System.Drawing.Point(456, 425);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(0, 13);
+            this.txtUsuario.TabIndex = 9;
+            // 
             // Cadastro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDataAlteracao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDataCadastro);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnSalvar);
             this.Name = "Cadastro";
-            this.Text = "Cadastro de Cidade";
+            this.Text = "Cadastro";
             this.Controls.SetChildIndex(this.txtCodigo, 0);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.btnSalvar, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtDataCadastro, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.txtDataAlteracao, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtUsuario, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +134,11 @@
         #endregion
 
         protected System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        protected System.Windows.Forms.Label txtDataCadastro;
+        protected System.Windows.Forms.Label txtDataAlteracao;
+        protected System.Windows.Forms.Label txtUsuario;
     }
 }
