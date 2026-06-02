4037f83581361ece5a60616fa513be9c69892091
over <ov3rline@proton.me>
2026-06-02 17:14:46 -0300

    Criar design Base

---
+++ Base.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Test
+{
+    partial class Base
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Codigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Codigo
+            // 
+            this.Codigo.Location = new System.Drawing.Point(15, 25);
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Size = new System.Drawing.Size(83, 20);
+            this.Codigo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Código";
+            // 
+            // Sair
+            // 
+            this.Sair.Location = new System.Drawing.Point(722, 416);
+            this.Sair.Name = "Sair";
+            this.Sair.Size = new System.Drawing.Size(66, 22);
+            this.Sair.TabIndex = 2;
+            this.Sair.Text = "Sair";
+            this.Sair.UseVisualStyleBackColor = true;
+            // 
+            // Base
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Sair);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Codigo);
+            this.Name = "Base";
+            this.Text = "Base";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Codigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Sair;
+    }
+}
